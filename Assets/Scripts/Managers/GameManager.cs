a0fff51386516f604c3cb992c392542377db0360
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2020-07-21 00:25:54 +0200

    Made a Reset function

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public State state;
+    public GameObject player;
     private static GameManager _instance;
          public static GameManager Instance {
          get {
@@ -73,5 +75,24 @@ public class GameManager : MonoBehaviour
     public void ReloadCurrentScene() 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void RemoveAllGameObjectsWithTag(string tag)
+    {
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag (tag);
+
+        foreach (var g in gameObjects)
+        {
+            Destroy(g);
+        }
+
+    }
+
+    public void Reset()
+    {
+        RemoveAllGameObjectsWithTag("Chunk");
+        player.transform.position = new Vector3(0f, -1.5f, 0f);
+        player.SetActive(true);
+        InGame();
     }
 }
