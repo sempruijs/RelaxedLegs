21b4feb544ed5e9d6ab96ffe29bcb13ba62bae4f
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2020-07-24 16:07:22 +0200

    Add chunk 8

---
+++ Assets/Scripts/Chunk/PlayAnimationCollideWithPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimationCollideWithPlayer : MonoBehaviour
+{
+    public Animator animator;
+    public string setBool;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool(setBool, true);    
+        }
+    }
+}
